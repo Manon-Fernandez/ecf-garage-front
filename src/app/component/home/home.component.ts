18333a85402144d96305737d5d529a7ffe38a68f
Manon Fernandez <fernandez.manon23230@gmail.com>
2023-07-11 19:00:26 +0200

    add fonction new avis

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {Avis} from "../../models/Avis.model";
 import {AvisService} from "../../services/avis/avis.service";
 import {Status} from "../../models/Avis.model";
+import {GarageServiceService} from "../../services/garageService/garage-service.service";
+import {Service} from "../../models/Service.model";
+import {AvisPopinComponent} from "../avis-popin/avis-popin.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-home',
@@ -11,9 +15,13 @@ import {Status} from "../../models/Avis.model";
 export class HomeComponent implements OnInit{
 
   avisList : Array<Avis>;
+  services : Array<Service>;
 
-  constructor(private avisService : AvisService) {
+  constructor(private avisService : AvisService,
+              private  garageService : GarageServiceService,
+              public dialog: MatDialog) {
     this.avisList = [];
+    this.services = [];
   }
 
   ngOnInit() {
@@ -22,7 +30,16 @@ export class HomeComponent implements OnInit{
        this.avisList.push(new Avis(unAvis));
       });
     });
+    this.garageService.getAllServices().subscribe(services => {
+      services.forEach(service => {
+        this.services.push(new Service(service));
+      })
+    })
   }
 
+
+  openPopinAvis(){
+    const dialogRef = this.dialog.open(AvisPopinComponent);
+  }
 
 }
