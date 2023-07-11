fb1bb45c9c299ba73c32850ec4ab3edcca8491b1
Manon Fernandez <fernandez.manon23230@gmail.com>
2023-07-11 17:53:49 +0200

    update home page

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Avis} from "../../models/Avis.model";
+import {AvisService} from "../../services/avis/avis.service";
+import {Status} from "../../models/Avis.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  avisList : Array<Avis>;
+
+  constructor(private avisService : AvisService) {
+    this.avisList = [];
+  }
+
+  ngOnInit() {
+    this.avisService.getAllAvisByStatus(Status.VALIDE).subscribe(avis => {
+      avis.forEach(unAvis => {
+       this.avisList.push(new Avis(unAvis));
+      });
+    });
+  }
+
 
 }
