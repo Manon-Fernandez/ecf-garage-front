acdcf9b2e7d6d3a823cab9d4098cf01c56da8c64
Manon Fernandez <fernandez.manon23230@gmail.com>
2023-07-11 17:56:28 +0200

    add services

---
+++ src/app/services/avis/avis.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Avis, Status} from "../../models/Avis.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AvisService {
+
+  url : string;
+  constructor(private httpClient: HttpClient) {
+    this.url = 'http://localhost:8081/api/avis';
+  }
+
+  getAllAvis() {
+    return this.httpClient.get<Array<Avis>>(this.url);
+  }
+
+  getAllAvisByStatus(status : Status){
+    return this.httpClient.get<Array<Avis>>(this.url + '/status/' + Status[status]);
+  }
+}
