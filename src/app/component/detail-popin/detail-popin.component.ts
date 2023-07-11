df87bfa996900622b17eaec05bfee09a6812b79d
Manon Fernandez <fernandez.manon23230@gmail.com>
2023-07-11 17:52:56 +0200

    add popin car

--- src/app/component/detail-popin/detail-popin.component.ts
+++ src/app/component/detail-popin/detail-popin.component.ts
@@ -1,24 +1,53 @@
-import {Component, Inject} from '@angular/core';
+import {Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Voiture} from "../../models/Voiture.model";
+import {Option} from "../../models/Option.model";
+import {OptionService} from "../../services/option/option.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-detail-popin',
   templateUrl: './detail-popin.component.html',
   styleUrls: ['./detail-popin.component.css']
 })
-export class DetailPopinComponent {
+export class DetailPopinComponent implements OnInit{
 
   voiture : Voiture;
+  options : Array<Option>;
+  isLoading : boolean;
 
   constructor(
     public dialogRef: MatDialogRef<DetailPopinComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MAT_DIALOG_DATA) public data: Voiture,
+    private optionService : OptionService,
+    private router : Router
   ) {
     this.voiture = new Voiture(data);
+    this.options = [];
+    this.isLoading = true;
   }
+
+  ngOnInit() {
+    this.optionService.getAllOptionByVoitureId(this.voiture.id).subscribe(response => {
+      response.forEach(uneOption => {
+        this.options.push(new Option(uneOption));
+      });
+      this.isLoading = false;
+    })
+  }
+
 
   onNoClick() {
     this.dialogRef.close();
   }
+
+  onContact(){
+    console.log(this.voiture.denomination);
+    this.router.navigate(['/contact'],
+      {queryParams: {
+        titleVoiture: this.voiture.denomination}})
+    this.dialogRef.close();
+  }
+
+
 }
